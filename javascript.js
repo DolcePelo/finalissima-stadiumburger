79fcbce8b998080a03648b4ecc0dfcfbee0b310f
Alejandro Pelozatto <alejandrojpelozatto@gmail.com>
2023-05-01 16:51:12 -0400

    Cree el carrousel, agregue un archivo javascript2

--- javascript.js
+++ javascript.js
@@ -1,22 +1,4 @@
 
-// const grande   =document.querySelector('.grande') 
-// const punto   =document.querySelectorAll('.punto')
-
-// punto.forEach ( ( cadaPunto, i) => {
-//     punto[i].addEventListener('click', ()=>{
-//         let posicion = i
-//         let operacion = posicion * -33
-
-//         grande.style.transform = `translateX(${ operacion }%)`
-
-//         punto.forEach( (cadaPunto, i)=>{
-//             punto[i].classList.remove('activo')
-//         } )
-//         punto[i].classList.add('activo')
-//     })
-// })
-
-
 var imagenes = [
                 "/assets/img/group-friends-watching-sport-together-bar.jpg",
                 "/assets/img/two-happy-female-friends-toasting-glass-cocktail.jpg",
